e386a0deff4dc40a6bc3b95184be6d69e1d97b29
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-03-16 17:17:15 +0000

    Add files

---
+++ src/main/java/com/github/myusername/Plugin.java
@@ -0,0 +1,13 @@
+package com.github.myusername;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
